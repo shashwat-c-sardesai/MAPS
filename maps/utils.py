3c6833f2034b671716c7d33b5b2ad4335710bd85
shashwat-c-sardesai <90284047+shashwat-c-sardesai@users.noreply.github.com>
2026-06-01 13:33:53 -0500

    Update utils.py
    Fixed error in angular power spectrum

--- maps/utils.py
+++ maps/utils.py
@@ -185,18 +185,23 @@ def angular_power_spectrum(clm):
         C_l: The angular power spectrum per spherical harmonic l.
     """
 
-    maxl = int(np.sqrt(clm.shape[0])) - 1
-    clm2 = clm ** 2
+    maxl = int(np.sqrt(clm.shape[0]))
+    new_clm2 = clm ** 2
 
-    C_l = np.zeros((maxl + 1))
+    C_l = np.zeros((maxl))
     idx = 0
 
-    for ll in range(maxl+1):
-        subset_len = 2*ll + 1
-        subset = np.arange(idx, idx + subset_len)
-
-        C_l[ll] = np.sum(clm2[subset]) / (2 * ll + 1)
-        idx = subset[-1] + 1
+    subsets = []
+    for ll in range(maxl):
+        if ll == 0:
+            C_l[ll] = new_clm2[ll]
+            idx += 1
+        else:
+            subset_len = 2 * ll + 1
+            subset = np.arange(idx, idx + subset_len)
+            subsets.append(subset)
+            C_l[ll] = np.sum(new_clm2[subset]) / (2 * ll + 1)
+            idx = subset[-1] + 1
 
     return C_l
 
